8b4b41ac620acd6bb0f620930b1db8218e5d6f50
Maria Kasińska <mariakasinska17@gmail.com>
2020-03-04 16:58:41 +0100

    fix endpoints

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // DONE GET /testimonials – ma po prostu zwracać całą zawartość tablicy.
 // DONE GET /testimonials/:id – zwracamy tylko jeden element tablicy, zgodny z :id.
-// GET /testimonials/random – zwracamy losowy element z tablicy.
+// DONE GET /testimonials/random – zwracamy losowy element z tablicy.
 // DONE POST /testimonials – dodajemy nowy element do tablicy. Możesz założyć, że body przekazywane przez klienta będzie obiektem z dwoma atrybutami author i text. Id dodawanego elementu musisz losować.
 // PUT /testimonials/:id – modyfikujemy atrybuty author i text elementu tablicy o pasującym :id. Załóż, że body otrzymane w requeście będzie obiektem z atrybutami author i text.
 // DELETE /testimonials/:id – usuwamy z tablicy wpis o podanym id.
@@ -19,12 +19,12 @@ app.get('/testimonials', (req, res) => {
   res.json(db);
 });
 
-app.get('/testimonials/:id', (req, res) => {
-  res.json(db.filter(item => item.id == req.params.id));
-});
-
 app.get('/testimonials/random', (req, res) => {
   res.json(db[Math.floor(Math.random() * db.length)]);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+  res.json(db.filter(item => item.id == req.params.id));
 });
 
 app.post('/testimonials', (req, res) => {
@@ -38,17 +38,21 @@ app.post('/testimonials', (req, res) => {
 });
 
 app.put('/testimonials/:id', (req, res) => {
-  console.log(req.params);
-  db.map(item =>
-    item.id == req.params.id
-      ? { ...item, author: req.body.author, text: req.body.text }
-      : item
-  );
+  const opinion = db.find(item => item.id == req.params.id);
+  const index = db.indexOf(opinion);
+  const updatedOpinion = {
+    ...opinion,
+    author: req.body.author,
+    text: req.body.text
+  };
+  db[index] = updatedOpinion;
   res.json({ message: 'OK' });
 });
 
 app.delete('/testimonials/:id', (req, res) => {
-  db.filter(item => item.id !== req.params.id);
+  const opinion = db.find(item => item.id == req.params.id);
+  const index = db.indexOf(opinion);
+  db.splice(index, 1);
   res.json({ message: 'OK' });
 });
 
