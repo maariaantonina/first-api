3d926de060fc7106ba4361eaf5c6467915d19262
Maria Kasińska <mariakasinska17@gmail.com>
2020-03-05 13:53:13 +0100

    add /concerts and /seats endpoints

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.use(express.json());
 // DELETE /testimonials/:id – usuwamy z tablicy wpis o podanym id.
 
 app.get('/testimonials', (req, res) => {
-  res.json(db);
+  res.json(db.testimonials);
 });
 
 app.get('/testimonials/random', (req, res) => {
-  res.json(db[Math.floor(Math.random() * db.length)]);
+  res.json(db.testimonials[Math.floor(Math.random() * db.testimonials.length)]);
 });
 
 app.get('/testimonials/:id', (req, res) => {
-  res.json(db.filter(item => item.id == req.params.id));
+  res.json(db.testimonials.filter(item => item.id == req.params.id));
 });
 
 app.post('/testimonials', (req, res) => {
@@ -33,26 +33,114 @@ app.post('/testimonials', (req, res) => {
     author: req.body.author,
     text: req.body.text
   };
-  db.push(newOpinion);
+  db.testimonials.push(newOpinion);
   res.json({ message: 'OK' });
 });
 
 app.put('/testimonials/:id', (req, res) => {
-  const opinion = db.find(item => item.id == req.params.id);
-  const index = db.indexOf(opinion);
+  const opinion = db.testimonials.find(item => item.id == req.params.id);
+  const index = db.testimonials.indexOf(opinion);
   const updatedOpinion = {
     ...opinion,
     author: req.body.author,
     text: req.body.text
   };
-  db[index] = updatedOpinion;
+  db.testimonials[index] = updatedOpinion;
   res.json({ message: 'OK' });
 });
 
 app.delete('/testimonials/:id', (req, res) => {
-  const opinion = db.find(item => item.id == req.params.id);
-  const index = db.indexOf(opinion);
-  db.splice(index, 1);
+  const opinion = db.testimonials.find(item => item.id == req.params.id);
+  const index = db.testimonials.indexOf(opinion);
+  db.testimonials.splice(index, 1);
+  res.json({ message: 'OK' });
+});
+
+//concerts
+
+app.get('/concerts', (req, res) => {
+  res.json(db.concerts);
+});
+
+app.get('/concerts/:id', (req, res) => {
+  res.json(db.concerts.filter(item => item.id == req.params.id));
+});
+
+app.post('/concerts', (req, res) => {
+  const newConcert = {
+    id: uuidv4(),
+    performer: req.body.performer,
+    genre: req.body.genre,
+    price: req.body.price,
+    day: req.body.day,
+    image: req.body.image
+  };
+  db.concerts.push(newConcert);
+  res.json({ message: 'OK' });
+});
+
+app.put('/concerts/:id', (req, res) => {
+  const concert = db.concerts.find(item => item.id == req.params.id);
+  const index = db.concerts.indexOf(concert);
+  const updatedConcert = {
+    ...concert,
+    performer: req.body.performer,
+    genre: req.body.genre,
+    price: req.body.price,
+    day: req.body.day,
+    image: req.body.image
+  };
+  db.concerts[index] = updatedConcert;
+  res.json({ message: 'OK' });
+});
+
+app.delete('/concerts/:id', (req, res) => {
+  const concert = db.concerts.find(item => item.id == req.params.id);
+  const index = db.concerts.indexOf(concert);
+  db.concerts.splice(index, 1);
+  res.json({ message: 'OK' });
+});
+
+// seats
+
+app.get('/seats', (req, res) => {
+  res.json(db.seats);
+});
+
+app.get('/seats/:id', (req, res) => {
+  res.json(db.seats.filter(item => item.id == req.params.id));
+});
+
+app.post('/seats', (req, res) => {
+  const newSeat = {
+    id: uuidv4(),
+    day: req.body.day,
+    seat: req.body.seat,
+    client: req.body.client,
+    email: req.body.email
+  };
+  db.seats.push(newSeat);
+  res.json({ message: 'OK' });
+});
+
+app.put('/seats/:id', (req, res) => {
+  const seat = db.seats.find(item => item.id == req.params.id);
+  const index = db.seats.indexOf(seat);
+  const updatedSeat = {
+    ...seat,
+    day: req.body.day,
+    seat: req.body.seat,
+    client: req.body.client,
+    email: req.body.email
+  };
+  db.seats[index] = updatedSeat;
+  res.json({ message: 'OK' });
+});
+
+app.delete('/seats/:id', (req, res) => {
+  const seat = db.seats.find(item => item.id == req.params.id);
+  const index = db.seats.indexOf(seat);
+  db.seats.splice(index, 1);
   res.json({ message: 'OK' });
 });
 
